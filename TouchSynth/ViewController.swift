996d0aa0a5524c0801e3cb57533af4389105597d
Stephanie Cleland <stephanie.e.cleland@gmail.com>
2015-02-16 16:50:16 -0500

    test

--- TouchSynth/ViewController.swift
+++ TouchSynth/ViewController.swift
@@ -6,20 +6,46 @@
 //  Copyright (c) 2015 Stephanie Cleland. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Controller: UISegmentedControl!
+ 
+    @IBOutlet weak var editView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func ChangeLabel(sender: AnyObject) {
+        if Controller.selectedSegmentIndex == 0 {
+            editView.hidden = true;
+        }
+        if Controller.selectedSegmentIndex == 1 {
+            editView.hidden = false;
+        }
+    }
+    
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        recognizer.view!.center = CGPoint(x:recognizer.view!.center.x + translation.x,
+            y:recognizer.view!.center.y + translation.y)
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+    }
+    
+    @IBAction func handleTap(recognizer:UITapGestureRecognizer) {
+        let alertController = UIAlertController(title: "Butoon", message: "Hi there", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
 }
-
