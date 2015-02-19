2b215170fdb08e0274047abe5a6d9593a5b1e585
bfisch02 <brett.fischler@tufts.edu>
2015-02-19 14:32:25 -0500

    Trying to add TouchSynthProject

---
+++ TouchSynthProject/TouchSynthProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TouchSynthProject
+//
+//  Created by Comp150 on 2/19/15.
+//  Copyright (c) 2015 Comp150. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
