2621d39a30319e019d23e794a0005c03a3b2c757
Comp150 <comp150@mlab5.music.tufts.edu>
2015-03-31 21:31:36 -0400

    Added seque to settings view controller

---
+++ TouchSynthProject/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  TouchSynthProject
+//
+//  Created by Comp150 on 3/31/15.
+//  Copyright (c) 2015 Comp150. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class SettingsViewController: UIViewController {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
